1cec8846592bc7e5e6726c76eb5a46e0deeaf2bf
Zentai Pál <zentaipali@gmail.com>
2023-05-02 09:26:34 +0200

    fejlec, tartalom

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+/*
+* File: App.js
+* Author: Zentai Pál
+* Copyright: 2023, Zentai Pál
+* Group: Szoft-II-N
+* Date: 2023-05-02
+* Github: https://github.com/Pali002/
+* Licenc: GNU GPL
+*/
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [bikes, setBikes] = useState([]);
+  function getBikes() {
+    let host = 'http://localhost:8000/';
+    let endpoint = 'bikes';
+    let url = host + endpoint;
+    
+    fetch(url).then(response => response.json())
+    .then(result => {
+      setBikes(result)
+    });
+  }
+
+  useEffect(() => {
+    getBikes();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gepida</Text>
+      <Text>05-02 dolgozat</Text>
+
+      <FlatList
+      data={bikes}
+      renderItem={ ({item}) => (
+        <Text>{item.name}</Text>
+      )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
